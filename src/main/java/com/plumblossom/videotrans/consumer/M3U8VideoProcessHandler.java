d728eefa5b4b19e45f64fd53e4981b6544b6da60
ZuoYanCoder <adm1nistrator@163.com>
2020-10-13 15:09:37 +0800

    只监听m3u8-h264-hq队列

--- src/main/java/com/plumblossom/videotrans/consumer/M3U8VideoProcessHandler.java
+++ src/main/java/com/plumblossom/videotrans/consumer/M3U8VideoProcessHandler.java
@@ -30,51 +30,6 @@ public class M3U8VideoProcessHandler {
 
     private static final String FILE_SUFFIX = ".m3u8";
 
-    @RabbitListener(queues = {RabbitmqConfig.QUEUE_M3U8_H264_FLUENT},containerFactory = "customContainerFactory")
-    public void receivedM3U8H264FluentProcessTask(String msg, Message message, Channel channel){
-        // 记录提交转码视频请求
-        log.info("[M3U8] [h264] [fluent] [command]:" + msg);
-        Map params = JSON.parseObject(msg, Map.class);
-        VideoTransFactory videoTransFactory = new M3U8VideoTransFactory();
-        VideoTrans videoTrans = videoTransFactory.getH264VideoTrans();
-
-        // 生成新的转码视频目录
-        String storage_path = (String) params.get(CommonConstValue.KEY_VIDEO_PARAMS_STORAGE_FOLDER_PATH);
-        String h264Path = storage_path + "M3U8"+File.separator + "H264" + File.separatorChar;
-        String newFileName = (String) params.get(CommonConstValue.KEY_VIDEO_PARAMS_NEW_FILE_NAME) + FILE_SUFFIX;
-        String fluentPath = h264Path +"fluent" +File.separator;
-        params.put(CommonConstValue.KEY_VIDEO_PARAMS_NEW_FILE_NAME, newFileName);
-        params.put(CommonConstValue.KEY_VIDEO_PARAMS_STORAGE_FOLDER_PATH,fluentPath);
-        params.put(CommonConstValue.KEY_VIDEO_PARAMS_NEW_VIDEO_PATH, fluentPath + newFileName);
-
-        String result = videoTrans.fluentTrans(params);
-        // 记录结果
-        log.info(msg + "["+result+"]");
-    }
-
-
-
-    @RabbitListener(queues = {RabbitmqConfig.QUEUE_M3U8_H264_HD},containerFactory = "customContainerFactory")
-    public void receivedM3U8H264HDProcessTask(String msg, Message message, Channel channel) {
-        // 记录提交转码视频请求
-        log.info("[M3U8] [h264] [hd] [command]:" + msg);
-        Map params = JSON.parseObject(msg, Map.class);
-        VideoTransFactory videoTransFactory = new M3U8VideoTransFactory();
-        VideoTrans videoTrans = videoTransFactory.getH264VideoTrans();
-
-        // 生成新的转码视频目录
-        String storage_path = (String) params.get(CommonConstValue.KEY_VIDEO_PARAMS_STORAGE_FOLDER_PATH);
-        String h264Path = storage_path + "M3U8" + File.separator + "H264" + File.separatorChar;
-        String newFileName = (String) params.get(CommonConstValue.KEY_VIDEO_PARAMS_NEW_FILE_NAME) + FILE_SUFFIX;
-        String hdPath = h264Path + "hd" + File.separator;
-        params.put(CommonConstValue.KEY_VIDEO_PARAMS_NEW_FILE_NAME, newFileName);
-        params.put(CommonConstValue.KEY_VIDEO_PARAMS_STORAGE_FOLDER_PATH, hdPath);
-        params.put(CommonConstValue.KEY_VIDEO_PARAMS_NEW_VIDEO_PATH, hdPath + newFileName);
-        String result = videoTrans.hdTrans(params);
-        // 记录结果
-        log.info(msg + "[" + result + "]");
-
-    }
 
 
 
@@ -100,77 +55,6 @@ public class M3U8VideoProcessHandler {
 
     }
 
-
-
-
-    @RabbitListener(queues = {RabbitmqConfig.QUEUE_M3U8_H265_FLUENT},containerFactory = "customContainerFactory")
-    public void receivedM3U8H265FluentProcessTask(String msg, Message message, Channel channel){
-        // 记录提交转码视频请求
-        log.info("[M3U8] [h265] [fluent] [command]:" + msg);
-        Map params = JSON.parseObject(msg, Map.class);
-        VideoTransFactory videoTransFactory = new M3U8VideoTransFactory();
-        VideoTrans videoTrans = videoTransFactory.getH265VideoTrans();
-
-        // 生成新的转码视频目录
-        String storage_path = (String) params.get(CommonConstValue.KEY_VIDEO_PARAMS_STORAGE_FOLDER_PATH);
-        String h265Path = storage_path + "M3U8"+File.separator + "H265" + File.separatorChar;
-        String newFileName = (String) params.get(CommonConstValue.KEY_VIDEO_PARAMS_NEW_FILE_NAME) + FILE_SUFFIX;
-        String fluentPath = h265Path +"fluent" +File.separator;
-        params.put(CommonConstValue.KEY_VIDEO_PARAMS_NEW_FILE_NAME, newFileName);
-        params.put(CommonConstValue.KEY_VIDEO_PARAMS_STORAGE_FOLDER_PATH,fluentPath);
-        params.put(CommonConstValue.KEY_VIDEO_PARAMS_NEW_VIDEO_PATH, fluentPath + newFileName);
-
-        String result = videoTrans.fluentTrans(params);
-        // 记录结果
-        log.info(msg + "["+result+"]");
-    }
-
-
-
-    @RabbitListener(queues = {RabbitmqConfig.QUEUE_M3U8_H265_HD},containerFactory = "customContainerFactory")
-    public void receivedM3U8H265HDProcessTask(String msg, Message message, Channel channel) {
-        // 记录提交转码视频请求
-        log.info("[M3U8] [h265] [hd] [command]:" + msg);
-        Map params = JSON.parseObject(msg, Map.class);
-        VideoTransFactory videoTransFactory = new M3U8VideoTransFactory();
-        VideoTrans videoTrans = videoTransFactory.getH265VideoTrans();
-
-        // 生成新的转码视频目录
-        String storage_path = (String) params.get(CommonConstValue.KEY_VIDEO_PARAMS_STORAGE_FOLDER_PATH);
-        String h265Path = storage_path + "M3U8" + File.separator + "H265" + File.separatorChar;
-        String newFileName = (String) params.get(CommonConstValue.KEY_VIDEO_PARAMS_NEW_FILE_NAME) + FILE_SUFFIX;
-        String hdPath = h265Path + "hd" + File.separator;
-        params.put(CommonConstValue.KEY_VIDEO_PARAMS_NEW_FILE_NAME, newFileName);
-        params.put(CommonConstValue.KEY_VIDEO_PARAMS_STORAGE_FOLDER_PATH, hdPath);
-        params.put(CommonConstValue.KEY_VIDEO_PARAMS_NEW_VIDEO_PATH, hdPath + newFileName);
-        String result = videoTrans.hdTrans(params);
-        // 记录结果
-        log.info(msg + "[" + result + "]");
-
-    }
-
-
-
-    @RabbitListener(queues = {RabbitmqConfig.QUEUE_M3U8_H265_HQ},containerFactory = "customContainerFactory")
-    public void receivedM3U8H265HQProcessTask(String msg, Message message, Channel channel) {
-        // 记录提交转码视频请求
-        log.info("[M3U8] [h265] [hq] [command]:" + msg);
-        Map params = JSON.parseObject(msg, Map.class);
-        VideoTransFactory videoTransFactory = new M3U8VideoTransFactory();
-        VideoTrans videoTrans = videoTransFactory.getH265VideoTrans();
-
-        // 生成新的转码视频目录
-        String storage_path = (String) params.get(CommonConstValue.KEY_VIDEO_PARAMS_STORAGE_FOLDER_PATH);
-        String h265Path = storage_path + "M3U8" + File.separator + "H265" + File.separatorChar;
-        String newFileName = (String) params.get(CommonConstValue.KEY_VIDEO_PARAMS_NEW_FILE_NAME) + FILE_SUFFIX;
-        String hqPath = h265Path + "hq" + File.separator;
-        params.put(CommonConstValue.KEY_VIDEO_PARAMS_NEW_FILE_NAME, newFileName);
-        params.put(CommonConstValue.KEY_VIDEO_PARAMS_STORAGE_FOLDER_PATH, hqPath);
-        params.put(CommonConstValue.KEY_VIDEO_PARAMS_NEW_VIDEO_PATH, hqPath + newFileName);
-        String result = videoTrans.hqTrans(params);
-        // 记录结果
-        log.info(msg + "[" + result + "]");
-    }
 
 
 }
