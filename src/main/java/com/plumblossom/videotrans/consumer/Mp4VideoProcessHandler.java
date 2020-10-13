2c02965c6ae6d449b272aee1c237dc608d6f66bf
ZuoYanCoder <adm1nistrator@163.com>
2020-10-13 15:17:48 +0800

    只监听mp4-h265-fluent队列

--- src/main/java/com/plumblossom/videotrans/consumer/Mp4VideoProcessHandler.java
+++ src/main/java/com/plumblossom/videotrans/consumer/Mp4VideoProcessHandler.java
@@ -30,80 +30,6 @@ public class Mp4VideoProcessHandler {
 
     private static final String FILE_SUFFIX = ".mp4";
 
-    @RabbitListener(queues = {RabbitmqConfig.QUEUE_MP4_H264_FLUENT},containerFactory = "customContainerFactory")
-    public void receivedMP4H264FluentProcessTask(String msg, Message message, Channel channel){
-        // 记录提交转码视频请求
-        log.info("[mp4] [h264] [fluent] [command]:" + msg);
-        Map params = JSON.parseObject(msg, Map.class);
-        VideoTransFactory videoTransFactory = new MP4VideoTransFactory();
-        VideoTrans videoTrans = videoTransFactory.getH264VideoTrans();
-
-        // 生成新的转码视频目录
-        String storage_path = (String) params.get(CommonConstValue.KEY_VIDEO_PARAMS_STORAGE_FOLDER_PATH);
-        String h264Path = storage_path + "Mp4"+File.separator + "H264" + File.separatorChar;
-        String newFileName = (String) params.get(CommonConstValue.KEY_VIDEO_PARAMS_NEW_FILE_NAME) + FILE_SUFFIX;
-        String fluentPath = h264Path +"fluent" +File.separator;
-
-        params.put(CommonConstValue.KEY_VIDEO_PARAMS_NEW_FILE_NAME, newFileName);
-        params.put(CommonConstValue.KEY_VIDEO_PARAMS_STORAGE_FOLDER_PATH,fluentPath);
-        params.put(CommonConstValue.KEY_VIDEO_PARAMS_NEW_VIDEO_PATH, fluentPath + newFileName );
-
-        String result = videoTrans.fluentTrans(params);
-        // 记录结果
-        log.info(msg + "["+result+"]");
-    }
-
-
-
-    @RabbitListener(queues = {RabbitmqConfig.QUEUE_MP4_H264_HD},containerFactory = "customContainerFactory")
-    public void receivedMP4H264HDProcessTask(String msg, Message message, Channel channel) {
-        // 记录提交转码视频请求
-        log.info("[mp4] [h264] [hd] [command]:" + msg);
-        Map params = JSON.parseObject(msg, Map.class);
-        VideoTransFactory videoTransFactory = new MP4VideoTransFactory();
-        VideoTrans videoTrans = videoTransFactory.getH264VideoTrans();
-
-        // 生成新的转码视频目录
-        String storage_path = (String) params.get(CommonConstValue.KEY_VIDEO_PARAMS_STORAGE_FOLDER_PATH);
-        String h264Path = storage_path + "Mp4" + File.separator + "H264" + File.separatorChar;
-        String newFileName = (String) params.get(CommonConstValue.KEY_VIDEO_PARAMS_NEW_FILE_NAME)+ FILE_SUFFIX;
-        String hdPath = h264Path + "hd" + File.separator;
-        params.put(CommonConstValue.KEY_VIDEO_PARAMS_NEW_FILE_NAME, newFileName);
-        params.put(CommonConstValue.KEY_VIDEO_PARAMS_STORAGE_FOLDER_PATH, hdPath);
-        params.put(CommonConstValue.KEY_VIDEO_PARAMS_NEW_VIDEO_PATH, hdPath + newFileName);
-        String result = videoTrans.hdTrans(params);
-        // 记录结果
-        log.info(msg + "[" + result + "]");
-
-    }
-
-
-
-    @RabbitListener(queues = {RabbitmqConfig.QUEUE_MP4_H264_HQ},containerFactory = "customContainerFactory")
-    public void receivedMP4H264HQProcessTask(String msg, Message message, Channel channel) {
-        // 记录提交转码视频请求
-        log.info("[mp4] [h264] [hq] [command]:" + msg);
-        Map params = JSON.parseObject(msg, Map.class);
-        VideoTransFactory videoTransFactory = new MP4VideoTransFactory();
-        VideoTrans videoTrans = videoTransFactory.getH264VideoTrans();
-
-        // 生成新的转码视频目录
-        String storage_path = (String) params.get(CommonConstValue.KEY_VIDEO_PARAMS_STORAGE_FOLDER_PATH);
-        String h264Path = storage_path + "Mp4" + File.separator + "H264" + File.separatorChar;
-        String newFileName = (String) params.get(CommonConstValue.KEY_VIDEO_PARAMS_NEW_FILE_NAME)+ FILE_SUFFIX;
-        String hqPath = h264Path + "hq" + File.separator;
-        params.put(CommonConstValue.KEY_VIDEO_PARAMS_NEW_FILE_NAME, newFileName);
-        params.put(CommonConstValue.KEY_VIDEO_PARAMS_STORAGE_FOLDER_PATH, hqPath);
-        params.put(CommonConstValue.KEY_VIDEO_PARAMS_NEW_VIDEO_PATH, hqPath + newFileName);
-        String result = videoTrans.hqTrans(params);
-        // 记录结果
-        log.info(msg + "[" + result + "]");
-
-    }
-
-
-
-
 
 
     @RabbitListener(queues = {RabbitmqConfig.QUEUE_MP4_H265_FLUENT},containerFactory = "customContainerFactory")
@@ -127,58 +53,6 @@ public class Mp4VideoProcessHandler {
         // 记录结果
         log.info(msg + "["+result+"]");
     }
-
-
-
-    @RabbitListener(queues = {RabbitmqConfig.QUEUE_MP4_H265_HD},containerFactory = "customContainerFactory")
-    public void receivedMP4H265HDProcessTask(String msg, Message message, Channel channel) {
-        // 记录提交转码视频请求
-        log.info("[mp4] [h265] [hd] [command]:" + msg);
-        Map params = JSON.parseObject(msg, Map.class);
-        VideoTransFactory videoTransFactory = new MP4VideoTransFactory();
-        VideoTrans videoTrans = videoTransFactory.getH265VideoTrans();
-
-        // 生成新的转码视频目录
-        String storage_path = (String) params.get(CommonConstValue.KEY_VIDEO_PARAMS_STORAGE_FOLDER_PATH);
-        String h265Path = storage_path + "Mp4" + File.separator + "H265" + File.separatorChar;
-        String newFileName = (String) params.get(CommonConstValue.KEY_VIDEO_PARAMS_NEW_FILE_NAME)+ FILE_SUFFIX;
-        String hdPath = h265Path + "hd" + File.separator;
-        params.put(CommonConstValue.KEY_VIDEO_PARAMS_NEW_FILE_NAME, newFileName);
-        params.put(CommonConstValue.KEY_VIDEO_PARAMS_STORAGE_FOLDER_PATH, hdPath);
-        params.put(CommonConstValue.KEY_VIDEO_PARAMS_NEW_VIDEO_PATH, hdPath + newFileName);
-        String result = videoTrans.hdTrans(params);
-        // 记录结果
-        log.info(msg + "[" + result + "]");
-
-    }
-
-
-
-    @RabbitListener(queues = {RabbitmqConfig.QUEUE_MP4_H265_HQ},containerFactory = "customContainerFactory")
-    public void receivedMP4H265HQProcessTask(String msg, Message message, Channel channel) {
-        // 记录提交转码视频请求
-        log.info("[mp4] [h265] [hq] [command]:" + msg);
-        Map params = JSON.parseObject(msg, Map.class);
-        VideoTransFactory videoTransFactory = new MP4VideoTransFactory();
-        VideoTrans videoTrans = videoTransFactory.getH265VideoTrans();
-
-        // 生成新的转码视频目录
-        String storage_path = (String) params.get(CommonConstValue.KEY_VIDEO_PARAMS_STORAGE_FOLDER_PATH);
-        String h265Path = storage_path + "Mp4" + File.separator + "H265" + File.separatorChar;
-        String newFileName = (String) params.get(CommonConstValue.KEY_VIDEO_PARAMS_NEW_FILE_NAME) + FILE_SUFFIX;
-        String hqPath = h265Path + "hq" + File.separator;
-        params.put(CommonConstValue.KEY_VIDEO_PARAMS_NEW_FILE_NAME, newFileName);
-        params.put(CommonConstValue.KEY_VIDEO_PARAMS_STORAGE_FOLDER_PATH, hqPath);
-        params.put(CommonConstValue.KEY_VIDEO_PARAMS_NEW_VIDEO_PATH, hqPath + newFileName);
-        String result = videoTrans.hqTrans(params);
-        // 记录结果
-        log.info(msg + "[" + result + "]");
-
-    }
-
-
-
-
 
 
 }
